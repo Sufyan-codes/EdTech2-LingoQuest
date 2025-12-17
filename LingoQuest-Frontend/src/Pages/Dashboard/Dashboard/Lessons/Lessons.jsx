3e53b3bfec0977e54f58c0c6dae592e5230f1f16
Abu-safyan yusuf <101343314+Sufyan-codes@users.noreply.github.com>
2025-12-17 12:22:42 +0100

    Update Lessons.jsx

--- LingoQuest-Frontend/src/Pages/Dashboard/Dashboard/Lessons/Lessons.jsx
+++ LingoQuest-Frontend/src/Pages/Dashboard/Dashboard/Lessons/Lessons.jsx
@@ -68,7 +68,7 @@ export default function Lessons() {
       <div className="max-w-5xl mx-auto">
         <div className="flex items-center justify-between mb-6">
           <div>
-            <h1 className="text-2xl font-bold">Language Lessons</h1>
+            <h1 className="text-2xl sm:text-sm font-bold">Language Lessons</h1>
             <p className="text-sm text-gray-600">
               Available lessons in {user?.targetLanguage || "your target language"}
             </p>
@@ -76,7 +76,7 @@ export default function Lessons() {
           <div>
             <button
               onClick={() => navigate("/dashboard")}
-              className="bg-[#FF8F87] text-white px-4 py-2 rounded-lg"
+              className="bg-[#FF8F87] text-white px-4 py-2 sm:text-sm rounded-lg"
             >
               Go to Dashboard
             </button>
