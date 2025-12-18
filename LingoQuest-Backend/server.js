d0c00ae4d4c60ecada10ddfa4286b9c3b0762eab
Abu-safyan yusuf <101343314+Sufyan-codes@users.noreply.github.com>
2025-12-18 10:44:25 +0100

    Update server.js

--- LingoQuest-Backend/server.js
+++ LingoQuest-Backend/server.js
@@ -23,6 +23,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://edtech2-lingoquest-mvp.netlify.app",
+    "https://lingoquest-pf28.onrender.com",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -67,6 +68,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 
